776f8d6161f6b73e71318ea747edeeb5f27f8272
Covaci Andndrei-Bogdan <andrei.covaci@icloud.com>
2026-03-12 22:50:09 +0200

    First steps to a 3rd Person

---
+++ Assets/PlayerController/FinalCharacterController/Scripts/PlayerController.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+
+namespace PlayerController.FinalCharacterController{
+    public class PlayerController : MonoBehaviour
+    {
+        
+    }
+}
---
+++ Assets/PlayerController/FinalCharacterController/Scripts/PlayerLocomotionInput.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+
+
+namespace PlayerController.FinalCharacterController{
+    public class NewMonoBehaviourScript1 : MonoBehaviour, PlayerControls.IPlayerLocomotionMapActions
+    {
+        public PlayerControls PlayerControls {get; private set; }
+        public Vector2 MovementInput {get; private set; }
+
+        private void OnEnable() 
+        {
+            PlayerControls = new PlayerControls(); 
+            PlayerControls.Enable();
+
+            PlayerControls.PlayerLocomotionMap.Enable();
+            PlayerControls.PlayerLocomotionMap.SetCallbacks(this);   
+        }
+
+        private void OnDisable() 
+        {
+            PlayerControls.PlayerLocomotionMap.Disable();
+            PlayerControls.PlayerLocomotionMap.RemoveCallbacks(this);    
+        }
+       
+        public void OnNewaction(InputAction.CallbackContext context)
+        {
+            MovementInput = context.ReadValue<Vector2>();
+            print(MovementInput);
+        }
+    }
+}
