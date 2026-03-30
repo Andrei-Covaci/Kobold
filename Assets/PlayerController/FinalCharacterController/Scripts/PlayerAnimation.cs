c3162c5f518a9013ab3351e86a2a859e0619fa9b
Covaci Andndrei-Bogdan <andrei.covaci@icloud.com>
2026-03-30 11:40:45 +0300

    TODO: Fix jump right

--- Assets/PlayerController/FinalCharacterController/Scripts/PlayerAnimation.cs
+++ Assets/PlayerController/FinalCharacterController/Scripts/PlayerAnimation.cs
@@ -24,6 +24,10 @@ namespace Kobold.FinalCharacterController
 
         private Vector3 _currentBlendInput = Vector3.zero;
 
+        private float _sprintMaxBlendValue = 1.5f;
+        private float _runMaxBlendValue = 1f;
+        private float _walkMaxBlendValue = 0.5f;
+
         private void Awake()
         {
             _playerLocomotionInput = GetComponent<PlayerLocomotionInput>();
@@ -45,9 +49,11 @@ namespace Kobold.FinalCharacterController
             bool isFalling = _playerState.CurrentPlayerMovementState == PlayerMovementState.Falling;
             bool isGrounded = _playerState.InGroundedState();
 
-            // Blend input for smoother transitions
-            Vector2 inputTarget = isSprinting ? _playerLocomotionInput.MovementInput * 1.5f: 
-                                  isRunning ? _playerLocomotionInput.MovementInput * 1f: _playerLocomotionInput.MovementInput * 0.5f;
+            bool isRunBlendValue = isRunning || isJumping || isFalling;
+            // Blend input for smoother transition
+            Vector2 inputTarget = isSprinting ? _playerLocomotionInput.MovementInput * _sprintMaxBlendValue: 
+                                  isRunBlendValue ? _playerLocomotionInput.MovementInput * _runMaxBlendValue:
+                                                    _playerLocomotionInput.MovementInput * _walkMaxBlendValue;
             _currentBlendInput = Vector3.Lerp(_currentBlendInput, inputTarget, locomotionBlendSpeed * Time.deltaTime);
             
             _animator.SetBool(isGroundedHash, isGrounded);
